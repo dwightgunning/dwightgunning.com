156685ece19bd6ad2b3b9b225012905682967c1a
Dwight Gunning <dwight@dwightgunning.com>
2023-01-02 16:21:59 -0600

    Added astro Image plugin and sharp optimizer dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
