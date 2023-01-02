d0ab70741cf1ce571f4fdfda4f9a616a064cbc6b
Dwight Gunning <dwight@dwightgunning.com>
2023-01-02 14:30:32 -0600

    Added tailwind css framework

---
+++ tailwind.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  plugins: [],
+};
