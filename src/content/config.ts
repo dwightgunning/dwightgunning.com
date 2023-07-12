05db85fa0f186b45c42af8150cb357f8a23e458b
Dwight Gunning <dwight@dwightgunning.com>
2023-07-12 09:32:58 +0200

    Switched Map thumbnail and placeholders to astro:assets Image component

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,39 @@
 import { z, defineCollection } from 'astro:content';
 
 const OutdoorsPagesCollection = defineCollection({
-  schema: z.object({
-    activityDate: z.date().optional(),
-    draft: z.boolean(),
-    introduction: z.string(),
-    label: z.string(),
-    metaDescription: z.string(),
-    map: z.object({
-      fullsize: z.object({
-        mapCenterDefault: z.object({
-          lat: z.number(),
-          lng: z.number(),
+  schema: ({ image }) =>
+    z.object({
+      activityDate: z.date().optional(),
+      draft: z.boolean(),
+      introduction: z.string(),
+      label: z.string(),
+      metaDescription: z.string(),
+      map: z.object({
+        fullsize: z.object({
+          image: image(),
+          mapCenterDefault: z.object({
+            lat: z.number(),
+            lng: z.number(),
+          }),
+          mapZoomDefault: z.number(),
         }),
-        mapZoomDefault: z.number(),
-      }),
-      thumbnail: z.object({
-        mapCenterDefault: z.object({
-          lat: z.number(),
-          lng: z.number(),
+        thumbnail: z.object({
+          image: image(),
+          mapCenterDefault: z.object({
+            lat: z.number(),
+            lng: z.number(),
+          }),
+          mapZoomDefault: z.number(),
         }),
-        mapZoomDefault: z.number(),
+        tracks: z.array(z.string()),
       }),
-      tracks: z.array(z.string()),
+      publishedAt: z.date().optional(),
+      tags: z.array(z.string()),
+      title: z.string(),
+      type: z.enum(['hiking', 'running', 'camping']),
+      wikipedia: z.string().optional(),
+      updatedAt: z.date().optional(),
     }),
-    publishedAt: z.date().optional(),
-    tags: z.array(z.string()),
-    title: z.string(),
-    type: z.enum(['hiking', 'running', 'camping']),
-    wikipedia: z.string().optional(),
-    updatedAt: z.date().optional(),
-  }),
 });
 
 export const collections = {
